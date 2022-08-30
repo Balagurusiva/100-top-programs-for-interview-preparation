d95f0f1c0578c192d2a625987b86015213005a0b
Balaguru <75660556+Balagurusiva@users.noreply.github.com>
2022-08-30 19:18:53 +0530

    Add files via upload

--- startup level/sumOfNUMs.java
+++ startup level/sumOfNUMs.java
@@ -28,14 +28,6 @@ public class sumOfNUMs{
     }
 
    //method 3 - using recursion
-   public class sumOfNUMs{
-    public static void main(String[] args){
-        
-        int num1= 2;
-        int num2 = 5;
-        int sum = num2*(num2+1)/2 - num1*(num1+1)/2 + num1;
-        System.out.println("the sum number in the range: " +sum);
-       }
-    }
+   
 
  */
